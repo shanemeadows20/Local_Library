44fd6ecbd51b477ef7eadc0c467a5d35bccd5e6a
Shane Meadows <75956896+shanemeadows20@users.noreply.github.com>
2021-01-03 17:02:52 -0800

    Add files via upload

---
+++ Project_Local_Library_1/public/src/accounts.js
@@ -0,0 +1,14 @@
+function findAccountById(accounts, id) {}
+
+function sortAccountsByLastName(accounts) {}
+
+function numberOfBorrows(account, books) {}
+
+function getBooksPossessedByAccount(account, books, authors) {}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  numberOfBorrows,
+  getBooksPossessedByAccount,
+};
---
+++ Project_Local_Library_1/public/src/home.js
@@ -0,0 +1,20 @@
+function totalBooksCount(books) {}
+
+function totalAccountsCount(accounts) {}
+
+function booksBorrowedCount(books) {}
+
+function getMostCommonGenres(books) {}
+
+function getMostPopularBooks(books) {}
+
+function getMostPopularAuthors(books, authors) {}
+
+module.exports = {
+  totalBooksCount,
+  totalAccountsCount,
+  booksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
